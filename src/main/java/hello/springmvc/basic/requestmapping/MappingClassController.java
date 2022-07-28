42a91b66d954f14cae7406a6577630500b569254
songsh927 <songsh96927@gmail.com>
2022-07-28 11:09:25 +0900

    22.07.28

---
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -0,0 +1,40 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/mapping/users")
+public class MappingClassController {
+    /*
+    회원 목록 조회: GET /users
+    회원 등록: POST /users
+    회원 조회: GET/users/{userId}
+    회원수정: PATCH /users/{userId}
+    회원 삭제: DELETE /users/{userId}
+     */
+
+    @GetMapping
+    public String user(){
+        return "get users";
+    }
+
+    @PostMapping
+    public String addUser(){
+        return "post user";
+    }
+
+    @GetMapping(value = "/{userId}")
+    public String findUser(@PathVariable String userId){
+        return "get userId " + userId;
+    }
+
+    @PatchMapping(value = "/{userId}")
+    public String updateUser(@PathVariable String userId){
+        return "update userId " + userId;
+    }
+
+    @DeleteMapping(value = "/{userId}")
+    public String deleteUser(@PathVariable String userId){
+        return "delete userId " + userId;
+    }
+}
